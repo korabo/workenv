46e046e4a963b7facba95ac19185014977513f37
korabocintake <takeuchi@korabo.jp>
2021-03-27 08:02:11 +0900

    Rev: fix when from powershell

--- platform/windows/opt/bin/hosts.cmd
+++ platform/windows/opt/bin/hosts.cmd
@@ -1,8 +1,8 @@
 @echo off
 REM https://qiita.com/yz2cm/items/8058d503a1b84688af09
 set _HOSTS_PATH_=C:\Windows\system32\drivers\etc
-if "%1"=="-h" (
-  set "_USG0_=Usage: %0 (<-h|-e> <notepad|sakura>)"
+if /I "%1" EQU "-h" (
+  set "_USG0_=Usage: %0 (<-h|-e|-v> <notepad|sakura>)"
   set "_USG1_=   -h:help, -e:edit"
   set "_USG2_=e.g.): %0 -h  -> show help"
   set "_USG3_=     : %0     -> open directory %_HOSTS_PATH_%"
@@ -19,6 +19,7 @@ if "%1"=="-h" (
 )
 
 if /I "%1" EQU ""         GOTO EXPL
+if /I "%1" EQU "-v"       GOTO VIEW
 if /I "%2" EQU "sakura"   GOTO SAKURA
 if /I "%2" EQU "notepad"  GOTO NOTEPAD
 
@@ -35,11 +36,16 @@ GOTO EDIT
 
 :EDIT
 echo "%_EDITOR_% %_HOSTS_PATH_%\hosts"
-start /B %_EDITOR_% %_HOSTS_PATH_%\hosts
+start /B sudo %_EDITOR_% "%_HOSTS_PATH_%\hosts"
 GOTO END
 
 :EXPL
 explorer %_HOSTS_PATH_%
+GOTO END
+
+:VIEW
+type "%_HOSTS_PATH_%\hosts"
+GOTO END
 
 :END
 @echo on
